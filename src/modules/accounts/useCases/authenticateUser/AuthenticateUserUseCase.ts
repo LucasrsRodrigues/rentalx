ec08dbc19c81c4ddec5e77539f72dfe37f57a576
lucas rodrigues <lucasrs.rodrigues3@outlook.com>
2021-04-13 11:01:31 -0300

    add upload avatar

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,61 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../errors/AppError";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  },
+  token: string;
+}
+
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ){}
+  
+  async execute({email, password}: IRequest): Promise<IResponse> {
+    // usuario existe
+    const user = await this.usersRepository.findByEmail(email);
+
+    if(!user){
+      throw new AppError("Email or password incorrect!");
+    }
+
+    // senha correta
+    const passwordMath = await compare(password,user.password);
+    
+    if(!passwordMath){
+      throw new AppError("Email or password incorrect!");
+    }
+
+    // gerar token
+    const token = sign({}, "fa96f96bf6a93806812593bfc74cf881", {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    const tokenReturn: IResponse = {
+      user: {
+        name: user.name,
+        email: user.email
+      },
+      token
+    }
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from 'swagger-ui-express';
 
-import { router } from './routes';
-
-import swaggerFile from './swagger.json';
 import './database';
+
+import "./shared/container";
+
+import { router } from './routes';
+import swaggerFile from './swagger.json';
+import { AppError } from "./errors/AppError";
 
 const app = express();
 
@@ -13,6 +17,19 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof AppError){
+    return response.status(err.statusCode).json({
+      message: err.message
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server Error - ${err.message}`
+  });
+});
 
 app.listen(3333, () => {
   console.log("Server started on port 3333!");
